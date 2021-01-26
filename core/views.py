24a578b73509f1e0e8f0d1c1fc68ab60da453dbf
Goosefrom <sanchoskorch765@gmail.com>
2021-01-26 13:41:29 +0200

    login done

--- core/views.py
+++ core/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
+
 class Index(LoginRequiredMixin, View):
     template = 'index.html'
     login_url = '/login/'
@@ -36,3 +37,18 @@ class Login(View):
             return HttpResponseRedirect('/')
         else:
             return render(request, self.template, {'form': form})
+
+
+class Password(View):
+    template = 'password.html'
+
+    def get(self, request):
+        return render(request, self.template)
+
+
+class Register(View):
+    template = 'register.html'
+
+    def get(self, request):
+        users = User.objects.all()
+        return render(request, self.template, {'users':users})
--- practic/urls.py
+++ practic/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Index.as_view(), name='index'),
     path('login/', views.Login.as_view(), name='login'),
+    path('password/', views.Password.as_view(), name='password'),
+    path('register/', views.Register.as_view(), name='register'),
     ]
