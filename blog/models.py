eb72d711fc7f8b10afbfc36da3f4177dfa96c3f8
Vladyslav <vladsheln58@gmail.com>
2021-05-27 23:13:27 +0300

    Accessability

--- blog/models.py
+++ blog/models.py
@@ -3,16 +3,18 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 import os
+import datetime
 
-BOOL_CHOICES = ((True, 'Yes'), (False, 'No'))
-
+# https://stackoverflow.com/questions/28166784/restricting-access-to-private-file-downloads-in-django
+# по ідеї захист від завантаження по url виконує сам http сервер, знайшов тільки як реалізувати на apache)
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	file = models.FileField(null=True,blank=True,upload_to='Files')
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	accessability = models.BooleanField(choices=BOOL_CHOICES, default=True)
+	accessability = models.BooleanField(default=True) # https://www.ordinarycoders.com/blog/article/using-django-form-fields-and-widgets
+	#day = models.DateField(initial=datetime.date.today)
 
 	def __str__(self):
 		return self.title
