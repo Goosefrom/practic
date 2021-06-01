34080a31eeabe4e94082900f560f6584278a34f3
Vladyslav <vladsheln58@gmail.com>
2021-06-01 21:22:26 +0300

    File required, anyway)

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ import datetime
 # по ідеї захист від завантаження по url виконує сам http сервер, знайшов тільки як реалізувати на apache)
 class Post(models.Model):
 	title = models.CharField(max_length=100)
-	file = models.FileField(null=True,blank=True,upload_to='Files')
+	file = models.FileField(null=False,blank=False,upload_to='Files')
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
