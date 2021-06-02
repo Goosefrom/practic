092567de11f2abb03f99382c97bfb9b3c679e837
Vladyslav <vladsheln58@gmail.com>
2021-06-02 15:27:41 +0300

    Google auth was fixed

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',# <-- https://simpleisbetterthancomplex.com/tutorial/2016/10/24/how-to-add-social-login-to-django.html
+    #all auth [google]
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -139,6 +145,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+SITE_ID = 2
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
