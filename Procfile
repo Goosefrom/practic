3747695a3c9f85af5571d4bfe9686ce13fc12878
Oleksandr Korchack <sanchoskorch765@gmail.com>
2021-06-03 19:50:38 +0300

    for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn practic.wsgi --log-file -
--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import dj-database-url
+
+db_from_env = dj-database-url.config()
+DATABASE['default'].update(db_from_env)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=m-e6oov5b2gv7o0af788&v5zgazj5-$x(67e$^tft^9y3-1x$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
