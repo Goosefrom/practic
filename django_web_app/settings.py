24b0a0739dbc46c860245093ec09452241b4391e
Oleksandr Korchack <sanchoskorch765@gmail.com>
2021-06-03 20:16:31 +0300

    blin

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-#import dj-database-url
+import dj-database-url
 
-#db_from_env = dj-database-url.config()
-#DATABASE['default'].update(db_from_env)
+db_from_env = dj-database-url.config()
+DATABASE['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
